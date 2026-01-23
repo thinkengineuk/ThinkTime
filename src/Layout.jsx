734ed053d814140e9ea49a0bc26abab96639b231
Ben Michaelis <ben@thinkengine.co>
2026-01-23 14:29:13 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -12,7 +12,7 @@ import {
   Menu,
   X
 } from "lucide-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
